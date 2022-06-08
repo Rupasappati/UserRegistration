8cf93e7d0f4c740f7df4c98215c2d855d9d7386c
RupaSappati <rupasappati@gmail.com>
2022-06-08 06:38:35 -0700

    Added: Lamda Expression

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -1,11 +1,87 @@
-namespace UnitTest1
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+//using UserRegistrationProblem;
+
+namespace UserRegistrationTesting
 {
     [TestClass]
     public class UnitTest1
     {
+
+        //Happy Test Cases(Test Cases Pass The Entries)
         [TestMethod]
-        public void TestMethod1()
+        public void GivenUserFistName_WhenValidate_ShouldReturnTrue()
         {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string firstName = "Akshay";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenUserLastName_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string lastName = "Sayre";
+            //Act
+            bool result = user.ValidateFirstName(lastName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string email = "akshaysayre7@gmail.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenMobileNumber_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string mobileNumber = "91 7999837990";
+            //Act
+            bool result = user.ValidateMobileNumber(mobileNumber);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenPassword_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string password = "Akshay@123";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void GivenEmailIds_WhenValidate_ShouldReturnTrue(string email)
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            //Act
+            bool result = user.ValidateEmail2(email);
+            //Assert
+            Assert.IsTrue(result);
         }
     }
 }
